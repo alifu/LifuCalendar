805d6392afe56b6d946c45e45ccc43d64a90f176
alifu <alif@docotel.co.id>
2018-12-26 14:54:28 +0800

    set project

---
+++ Example/Pods/Target Support Files/LifuCalendar/LifuCalendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LifuCalendar : NSObject
+@end
+@implementation PodsDummy_LifuCalendar
+@end
---
+++ Example/Pods/Target Support Files/Pods-LifuCalendar_Example/Pods-LifuCalendar_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LifuCalendar_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LifuCalendar_Example
+@end
